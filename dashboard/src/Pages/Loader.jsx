3e61f9f803aa7604db4fde7b35a586cd111624e3
Suhail Khan <khansohail015@gmail.com>
2024-05-14 10:17:06 +0530

    Update Loader.jsx

--- dashboard/src/Pages/Loader.jsx
+++ dashboard/src/Pages/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
     return (
         <div style={{ width: "50%", margin: " 120px auto",textAlign:"center"}}>
             <Spin size="large" />
-            <h1 style={{textAlign: "center", marginTop: "10px"}}>Please Wait.........</h1>
+            <h1 style={{textAlign: "center", marginTop: "10px"}}>Please Wait or Refresh Page.........</h1>
 
         </div>
     )
